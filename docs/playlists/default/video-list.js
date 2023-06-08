bb678967aba0bbb209994e770045d8166a11211f
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-08 19:22:05 -0400

    Add files via upload

---
+++ docs/playlists/default/video-list.js
@@ -0,0 +1,32 @@
+let allVideos = [
+   {
+      name: "Beginner's Guide",
+      src: "https://secure.archives.cafe/anny/43582013261.mp4",
+      id: "vid_1"
+   },
+   {
+      name: "Mario Party Collab w/Arex, Bella, and Bobber",
+      src: "https://secure.archives.cafe/anny/44299085085.mp4",
+      id: "vid_2"
+   },
+   {
+      name: "Superliminal (Single Player)",
+      src: "https://secure.archives.cafe/anny/44368611229.mp4",
+      id: "vid_3"
+   },
+   {
+      name: "There Is No Game (Part 1)",
+      src: "https://secure.archives.cafe/anny/44540038397.mp4",
+      id: "vid_4"
+   },
+   {
+      name: "There Is No Game (Part 2)",
+      src: "https://secure.archives.cafe/anny/44551900573.mp4",
+      id: "vid_5"
+   },
+   {
+      name: "Minecraft Vshojo Server (Part 7)",
+      src: "https://secure.archives.cafe/anny/44209167917.mp4",
+      id: "vid_6"
+   }
+]
