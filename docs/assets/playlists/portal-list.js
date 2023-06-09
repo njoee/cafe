970a543dc255a9f8a43afcfe08f05a4ec25ec580
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-09 18:52:13 -0400

    Create portal-list.js

---
+++ docs/assets/playlists/portal-list.js
@@ -0,0 +1,22 @@
+let allVideos = [
+    {
+        name: "Portal (Part 1)",
+        src: "https://secure.archives.cafe/anny/41622345597.mp4#t=5198",
+        id: "vod_1"   
+     },
+     {
+        name: "Portal (Part 2)",
+        src: "https://secure.archives.cafe/anny/41635418893.mp4",
+        id: "vod_2"   
+     },
+     {
+        name: "Portal 2 (Part 1)",
+        src: "https://secure.archives.cafe/anny/41679706157.mp4",
+        id: "vod_3"   
+     },
+     {
+        name: "Portal 2 (Part 2)",
+        src: "https://secure.archives.cafe/anny/39310982331.mp4",
+        id: "vod_4"   
+     }
+]
