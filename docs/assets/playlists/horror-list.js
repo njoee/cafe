3de52ac26134d507998d31c53acb61146b10a87e
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-09 18:22:52 -0400

    Create horror-list.js

---
+++ docs/assets/playlists/horror-list.js
@@ -0,0 +1,47 @@
+let allVideos = [
+   {
+      name: "Little Nightmares (Part 1)",
+      src: "https://secure.archives.cafe/anny/42143569438.mp4",
+      id: "vod_1"   
+   },
+   {
+      name: "Little Nightmares (Part 2) + Little Nightmares II (Part 1)",
+      src: "https://secure.archives.cafe/anny/41237468157.mp4",
+      id: "vod_2"   
+   },
+   {
+      name: "Little Nightmares II (Part 2)",
+      src: "https://secure.archives.cafe/anny/42220771934.mp4",
+      id: "vod_3"   
+   },
+   {
+      name: "Little Nightmares II (Part 3)",
+      src: "https://secure.archives.cafe/anny/39557646267.mp4",
+      id: "vod_4"   
+   },
+   {
+      name: "Little Nightmares II (Part 4)",
+      src: "https://secure.archives.cafe/anny/39562907099.mp4",
+      id: "vod_5"   
+   },
+   {
+      name: "SOMA (Part 1)",
+      src: "https://secure.archives.cafe/anny/39310293287.mp4",
+      id: "vod_6"
+   },
+   {
+      name: "SOMA (Part 2)",
+      src: "https://secure.archives.cafe/anny/39317699831.mp4",
+      id: "vod_7"
+   },
+   {
+      name: "SOMA (Part 3)",
+      src: "https://secure.archives.cafe/anny/39801264263.mp4",
+      id: "vod_8"
+   },
+   {
+      name: "SOMA (Part 4)",
+      src: "https://secure.archives.cafe/anny/39809289751.mp4",
+      id: "vod_9"
+   }
+]
