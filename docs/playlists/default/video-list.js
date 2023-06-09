d801afb0c38f24bdb5b0b974e3621958e13473e8
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-09 05:57:51 -0400

    Update video-list.js

--- docs/playlists/default/video-list.js
+++ docs/playlists/default/video-list.js
@@ -2,31 +2,116 @@ let allVideos = [
    {
       name: "Beginner's Guide",
       src: "https://secure.archives.cafe/anny/43582013261.mp4",
-      id: "vid_1"
+      poster: "https://secure.archives.cafe/anny/43582013261.jpg",
+      id: "vod_1"
+   },
+   {
+      name: "Firewatch",
+      src: "https://secure.archives.cafe/anny/45545911357.mp4",
+      poster: "https://secure.archives.cafe/anny/45545911357.jpg",
+      id: "vod_2"
+   },
+   {
+      name: "House Flipper",
+      src: "https://secure.archives.cafe/anny/45513028141.mp4",
+      poster: "https://secure.archives.cafe/anny/45513028141.jpg",
+      id: "vod_3"
+   },
+   {
+      name: "BIG BRAIN TIME WITH HAWOOKA (Escape Sim, Superliminal, Minecraft)",
+      src: "https://secure.archives.cafe/anny/44677238525.mp4",
+      poster: "https://secure.archives.cafe/anny/44677238525.jpg",
+      id: "vod_4"
    },
    {
       name: "Mario Party Collab w/Arex, Bella, and Bobber",
       src: "https://secure.archives.cafe/anny/44299085085.mp4",
-      id: "vid_2"
+      poster: "https://secure.archives.cafe/anny/44299085085.jpg",
+      id: "vod_5"
+   },
+   {
+      name: "Portal (Part 1)",
+      src: "https://secure.archives.cafe/anny/41622345597.mp4",
+      id: "vod_6"   
+   },
+   {
+      name: "Portal (Part 2)",
+      src: "https://secure.archives.cafe/anny/41635418893.mp4",
+      id: "vod_7"   
+   },
+   {
+      name: "Portal 2 (Part 1)",
+      src: "https://secure.archives.cafe/anny/41679706157.mp4",
+      id: "vod_8"   
+   },
+   {
+      name: "Portal 2 (Part 2)",
+      src: "https://secure.archives.cafe/anny/39310982331.mp4",
+      id: "vod_9"   
+   },
+   {
+      name: "Little Nightmares (Part 1)",
+      src: "https://secure.archives.cafe/anny/42143569438.mp4",
+      id: "vod_10"   
+   },
+   {
+      name: "Little Nightmares (Part 2) + Little Nightmares II (Part 1)",
+      src: "https://secure.archives.cafe/anny/41237468157.mp4",
+      id: "vod_11"   
+   },
+   {
+      name: "Little Nightmares II (Part 2)",
+      src: "https://secure.archives.cafe/anny/42220771934.mp4",
+      id: "vod_12"   
+   },
+   {
+      name: "Little Nightmares II (Part 3)",
+      src: "https://secure.archives.cafe/anny/39557646267.mp4",
+      id: "vod_13"   
+   },
+   {
+      name: "Little Nightmares II (Part 4)",
+      src: "https://secure.archives.cafe/anny/39562907099.mp4",
+      id: "vod_14"   
    },
    {
       name: "Superliminal (Single Player)",
       src: "https://secure.archives.cafe/anny/44368611229.mp4",
-      id: "vid_3"
+      id: "vod_15"
+   },
+   {
+      name: "SOMA (Part 1)",
+      src: "https://secure.archives.cafe/anny/39310293287.mp4",
+      id: "vod_16"
+   },
+   {
+      name: "SOMA (Part 2)",
+      src: "https://secure.archives.cafe/anny/39317699831.mp4",
+      id: "vod_17"
+   },
+   {
+      name: "SOMA (Part 3)",
+      src: "https://secure.archives.cafe/anny/39801264263.mp4",
+      id: "vod_18"
+   },
+   {
+      name: "SOMA (Part 4)",
+      src: "https://secure.archives.cafe/anny/39809289751.mp4",
+      id: "vod_19"
    },
    {
       name: "There Is No Game (Part 1)",
       src: "https://secure.archives.cafe/anny/44540038397.mp4",
-      id: "vid_4"
+      id: "vod_20"
    },
    {
       name: "There Is No Game (Part 2)",
       src: "https://secure.archives.cafe/anny/44551900573.mp4",
-      id: "vid_5"
+      id: "vod_21"
    },
    {
       name: "Minecraft Vshojo Server (Part 7)",
       src: "https://secure.archives.cafe/anny/44209167917.mp4",
-      id: "vid_6"
+      id: "vod_22"
    }
 ]
