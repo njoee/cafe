7136dd399129c75a3bc810a0abbbdde21172e524
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-09 18:23:51 -0400

    Create collab-list.js

---
+++ docs/assets/playlists/collab-list.js
@@ -0,0 +1,19 @@
+let allVideos = [
+   {
+      name: "BIG BRAIN TIME WITH HAWOOKA (Escape Sim, Superliminal, Minecraft)",
+      src: "https://secure.archives.cafe/anny/44677238525.mp4",
+      poster: "https://secure.archives.cafe/anny/44677238525.jpg",
+      id: "vod_1"
+   },
+   {
+      name: "Mario Party Collab w/Arex, Bella, and Bobber",
+      src: "https://secure.archives.cafe/anny/44299085085.mp4",
+      poster: "https://secure.archives.cafe/anny/44299085085.jpg",
+      id: "vod_2"
+   },
+   {
+      name: "Minecraft Vshojo Server (Part 7)",
+      src: "https://secure.archives.cafe/anny/44209167917.mp4",
+      id: "vod_3"
+   }
+]
