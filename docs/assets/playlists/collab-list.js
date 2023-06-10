0af3648ed25580af60033ad57217cda087ba1407
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-10 19:27:02 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -6,14 +6,20 @@ let allVideos = [
       id: "vod_1"
    },
    {
+      name: "look what Tob taught me peepoFinger (We Were Here)",
+      src: "https://secure.archives.cafe/anny/39935145031.mp4",
+      poster: "https://secure.archives.cafe/anny/39935145031.jpg",
+      id: "vod_2"
+   },
+   {
       name: "Mario Party Collab w/Arex, Bella, and Bobber",
       src: "https://secure.archives.cafe/anny/44299085085.mp4",
       poster: "https://secure.archives.cafe/anny/44299085085.jpg",
-      id: "vod_2"
+      id: "vod_3"
    },
    {
       name: "Minecraft Vshojo Server (Part 7)",
       src: "https://secure.archives.cafe/anny/44209167917.mp4",
-      id: "vod_3"
+      id: "vod_4"
    }
 ]
