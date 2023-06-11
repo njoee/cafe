effd04f386eef6a7275a9ed71b36d494e3073512
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-11 14:15:06 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,55 +1,43 @@
 let allVideos = [
    {
-      name: "Beginner's Guide",
-      src: "https://secure.archives.cafe/anny/43582013261.mp4",
-      poster: "https://secure.archives.cafe/anny/43582013261.jpg",
-      id: "vod_1"
-   },
-   {
-      name: "Firewatch",
-      src: "https://secure.archives.cafe/anny/45545911357.mp4#t=3629",
-      poster: "https://secure.archives.cafe/anny/45545911357.jpg",
-      id: "vod_2"
-   },
-   {
-      name: "Unpacking",
-      src: "https://secure.archives.cafe/anny/44346108365.mp4?824780683053367306#t=915",
-      poster: "https://secure.archives.cafe/anny/44346108365.jpg",
-      id: "vod_3"
-   },
-   {
-      name: "House Flipper",
-      src: "https://secure.archives.cafe/anny/45513028141.mp4",
-      poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_4"
-   },
-   {
-      name: "TUNIC (Part 1)",
-      src: "https://secure.archives.cafe/anny/45963158029.mp4",
-      poster: "https://secure.archives.cafe/anny/45963158029.jpg",
-      id: "vod_5"
-   },
-   {
-      name: "TUNIC (Part 2)",
-      src: "https://secure.archives.cafe/anny/45974743149.mp4",
-      poster: "https://secure.archives.cafe/anny/45974743149.jpg",
-      id: "vod_6"
-   },
-   {
       name: "TUNIC (Part 3)",
       src: "https://secure.archives.cafe/anny/45997436141.mp4",
       poster: "https://secure.archives.cafe/anny/45997436141.jpg",
-      id: "vod_7"
+      id: "vod_1"
    },
    {
       name: "TUNIC (Part 4)",
       src: "https://secure.archives.cafe/anny/39300866663.mp4",
       poster: "https://secure.archives.cafe/anny/39300866663.jpg",
-      id: "vod_8"
+      id: "vod_2"
+   },
+   {
+      name: "Beginner's Guide",
+      src: "https://secure.archives.cafe/anny/43582013261.mp4",
+      poster: "https://secure.archives.cafe/anny/43582013261.jpg",
+      id: "vod_3"
+   },
+   {
+      name: "Firewatch",
+      src: "https://secure.archives.cafe/anny/45545911357.mp4#t=3629",
+      poster: "https://secure.archives.cafe/anny/45545911357.jpg",
+      id: "vod_4"
+   },
+   {
+      name: "Unpacking",
+      src: "https://secure.archives.cafe/anny/44346108365.mp4?824780683053367306#t=915",
+      poster: "https://secure.archives.cafe/anny/44346108365.jpg",
+      id: "vod_5"
+   },
+   {
+      name: "House Flipper",
+      src: "https://secure.archives.cafe/anny/45513028141.mp4",
+      poster: "https://secure.archives.cafe/anny/45513028141.jpg",
+      id: "vod_6"
    },
    {
     name: "Superliminal (Single Player)",
     src: "https://secure.archives.cafe/anny/44368611229.mp4",
-    id: "vod_9"
+    id: "vod_7"
    }
 ]
