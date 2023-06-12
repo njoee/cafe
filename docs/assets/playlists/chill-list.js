b0c3f44f3c86a7e53c1337792522147791ad6e27
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-12 17:56:26 -0400

    Update chill-list.js
    fixes, added goose game with stopping point

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -3,23 +3,29 @@ let allVideos = [
       name: "Firewatch",
       src: "https://secure.archives.cafe/anny/45545911357.mp4#t=8465",
       poster: "https://secure.archives.cafe/anny/45545911357.jpg",
-      id: "vod_4"
+      id: "vod_1"
    },
    {
       name: "Unpacking",
       src: "https://secure.archives.cafe/anny/44346108365.mp4?824780683053367306#t=915",
       poster: "https://secure.archives.cafe/anny/44346108365.jpg",
-      id: "vod_5"
+      id: "vod_2"
    },
    {
       name: "House Flipper",
       src: "https://secure.archives.cafe/anny/45513028141.mp4",
       poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_6"
+      id: "vod_3"
    },
    {
     name: "Superliminal (Single Player)",
     src: "https://secure.archives.cafe/anny/44368611229.mp4",
-    id: "vod_7"
+    id: "vod_4"
+   },
+   {
+      name: "Untitled Goose Game",
+      src: "https://secure.archives.cafe/anny/44656209213.mp4#t=801,15807",
+      poster: "https://secure.archives.cafe/anny/44656209213.jpg",
+      id: "vod_5"
    }
 ]
