6874acef4cf34b445e81370fee3039365065c5cb
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-14 19:49:01 -0400

    Update portal-list.js

--- docs/assets/playlists/portal-list.js
+++ docs/assets/playlists/portal-list.js
@@ -6,17 +6,17 @@ let allVideos = [
      },
      {
         name: "Portal (Part 2)",
-        src: "https://secure.archives.cafe/anny/41635418893.mp4",
+        src: "https://secure.archives.cafe/anny/41635418893.mp4#t=4891",
         id: "vod_2"   
      },
      {
         name: "Portal 2 (Part 1)",
-        src: "https://secure.archives.cafe/anny/41679706157.mp4",
+        src: "https://secure.archives.cafe/anny/41679706157.mp4#t=1620",
         id: "vod_3"   
      },
      {
         name: "Portal 2 (Part 2)",
-        src: "https://secure.archives.cafe/anny/39310982331.mp4",
+        src: "https://secure.archives.cafe/anny/39310982331.mp4#t=2235",
         id: "vod_4"   
      }
 ]
