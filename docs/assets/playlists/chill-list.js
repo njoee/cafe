0681489d2c654d96db95eb02ff5e8e286a8cc9fc
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-14 19:24:35 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,19 +1,20 @@
 let allVideos = [
    {
-      name: "House Flipper",
-      src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
-      poster: "https://secure.archives.cafe/anny/45513028141.jpg",
+      name: "Detroit: Become Human 1",
+      src: "https://secure.archives.cafe/anny/43594282717.mp4#t=1272",
+      poster: "https://secure.archives.cafe/anny/43594282717.jpg",
       id: "vod_1"
    },
    {
-    name: "Superliminal (Single Player)",
-    src: "https://secure.archives.cafe/anny/44368611229.mp4#t=1560",
-    id: "vod_2"
+      name: "Detroit: Become Human 2",
+      src: "https://secure.archives.cafe/anny/43608427341.mp4#t=706",
+      poster: "https://secure.archives.cafe/anny/43608427341.jpg",
+      id: "vod_2"
    },
    {
-      name: "Untitled Goose Game",
-      src: "https://secure.archives.cafe/anny/44656209213.mp4#t=801,15807",
-      poster: "https://secure.archives.cafe/anny/44656209213.jpg",
+      name: "House Flipper",
+      src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
+      poster: "https://secure.archives.cafe/anny/45513028141.jpg",
       id: "vod_3"
-   }
+   },
 ]
