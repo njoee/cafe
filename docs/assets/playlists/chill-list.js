07ab44f958376564954e656ae76b3aee32b04f36
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-14 19:38:51 -0400

    Update chill-list.js
    added powerwash sim

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -12,9 +12,15 @@ let allVideos = [
       id: "vod_2"
    },
    {
+      name: "Powerwash Simulator",
+      src: "https://secure.archives.cafe/anny/45498742685.mp4#t=3836",
+      poster: "https://secure.archives.cafe/anny/45498742685.jpg",
+      id: "vod_3"
+   },
+   {
       name: "House Flipper",
       src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
       poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_3"
+      id: "vod_4"
    },
 ]
