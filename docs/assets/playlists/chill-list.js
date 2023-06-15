a5d87866050639632a1f2353e65b93219b95c021
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-15 08:29:32 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,26 +1,20 @@
 let allVideos = [
    {
-      name: "Detroit: Become Human 1",
-      src: "https://secure.archives.cafe/anny/43594282717.mp4#t=1272",
-      poster: "https://secure.archives.cafe/anny/43594282717.jpg",
-      id: "vod_1"
-   },
-   {
       name: "Detroit: Become Human 2",
-      src: "https://secure.archives.cafe/anny/43608427341.mp4#t=706",
+      src: "https://secure.archives.cafe/anny/43608427341.mp4#t=21400",
       poster: "https://secure.archives.cafe/anny/43608427341.jpg",
-      id: "vod_2"
+      id: "vod_1"
    },
    {
       name: "Powerwash Simulator",
       src: "https://secure.archives.cafe/anny/45498742685.mp4#t=3836",
       poster: "https://secure.archives.cafe/anny/45498742685.jpg",
-      id: "vod_3"
+      id: "vod_2"
    },
    {
       name: "House Flipper",
       src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
       poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_4"
+      id: "vod_3"
    },
 ]
