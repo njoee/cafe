9fa1ef63d129becc4575938bdaa6475ab2b2013f
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-16 18:00:20 -0400

    Update chill-list.js
    added deltarune, removed watched

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,44 +1,50 @@
 let allVideos = [
    {
-      name: "Helltaker",
-      src: "https://secure.archives.cafe/anny/41154806957.mp4#t=17545",
-      poster: "https://secure.archives.cafe/anny/41154806957.jpg",
-      id: "vod_1"
-   },
-   {
-      name: "Undertale Gen Run 1",
-      src: "https://secure.archives.cafe/anny/39325924123.mp4#t=2283",
-      poster: "https://secure.archives.cafe/anny/39325924123.jpg",
-      id: "vod_2"
-   },
-   {
-      name: "Undertale Gen Run 2",
-      src: "https://secure.archives.cafe/anny/39339734795.mp4#t=1479",
-      poster: "https://secure.archives.cafe/anny/39339734795.jpg",
-      id: "vod_3"
-   },
-   {
       name: "Undertale Gen Run 3",
-      src: "https://secure.archives.cafe/anny/39345137339.mp4#t=3596",
+      src: "https://secure.archives.cafe/anny/39345137339.mp4#t=6395",
       poster: "https://secure.archives.cafe/anny/39345137339.jpg",
-      id: "vod_4"
+      id: "vod_1"
    },
    {
       name: "Undertale Gen Run 4",
       src: "https://secure.archives.cafe/anny/39355600347.mp4#t=2333",
       poster: "https://secure.archives.cafe/anny/39355600347.jpg",
+      id: "vod_2"
+   },
+   {
+      name: "Deltarune Part 1",
+      src: "https://secure.archives.cafe/anny/1226716557.mp4",
+      poster: "https://secure.archives.cafe/anny/1226716557.jpg",
+      id: "vod_3"
+   },
+   {
+      name: "Deltarune Part 2",
+      src: "https://secure.archives.cafe/anny/39429041207.mp4#t=4444",
+      poster: "https://secure.archives.cafe/anny/39429041207.jpg",
+      id: "vod_4"
+   },
+   {
+      name: "Deltarune Part 3",
+      src: "https://secure.archives.cafe/anny/39431588679.mp4#t=4020",
+      poster: "https://secure.archives.cafe/anny/39431588679.jpg",
       id: "vod_5"
+   },
+   {
+      name: "Deltarune Part 4",
+      src: "https://secure.archives.cafe/anny/39438352343.mp4#t=2113",
+      poster: "https://secure.archives.cafe/anny/39438352343.jpg",
+      id: "vod_6"
    },
    {
       name: "Powerwash Simulator",
       src: "https://secure.archives.cafe/anny/45498742685.mp4#t=3836",
       poster: "https://secure.archives.cafe/anny/45498742685.jpg",
-      id: "vod_6"
+      id: "vod_7"
    },
    {
       name: "House Flipper",
       src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
       poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_7"
+      id: "vod_8"
    },
 ]
