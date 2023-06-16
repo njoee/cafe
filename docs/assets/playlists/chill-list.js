c9b3454d4fb8fc7bde611941ae1780f71979f2ba
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-15 20:14:34 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,20 +1,44 @@
 let allVideos = [
    {
-      name: "Detroit: Become Human 2",
-      src: "https://secure.archives.cafe/anny/43608427341.mp4#t=21400",
-      poster: "https://secure.archives.cafe/anny/43608427341.jpg",
+      name: "Helltaker",
+      src: "https://secure.archives.cafe/anny/41154806957.mp4#t=17545",
+      poster: "https://secure.archives.cafe/anny/41154806957.jpg",
       id: "vod_1"
+   },
+   {
+      name: "Undertale Gen Run 1",
+      src: "https://secure.archives.cafe/anny/39325924123.mp4#t=2283",
+      poster: "https://secure.archives.cafe/anny/39325924123.jpg",
+      id: "vod_2"
+   },
+   {
+      name: "Undertale Gen Run 2",
+      src: "https://secure.archives.cafe/anny/39339734795.mp4#t=1479",
+      poster: "https://secure.archives.cafe/anny/39339734795.jpg",
+      id: "vod_3"
+   },
+   {
+      name: "Undertale Gen Run 3",
+      src: "https://secure.archives.cafe/anny/39345137339.mp4#t=3596",
+      poster: "https://secure.archives.cafe/anny/39345137339.jpg",
+      id: "vod_4"
+   },
+   {
+      name: "Undertale Gen Run 4",
+      src: "https://secure.archives.cafe/anny/39355600347.mp4#t=2333",
+      poster: "https://secure.archives.cafe/anny/39355600347.jpg",
+      id: "vod_5"
    },
    {
       name: "Powerwash Simulator",
       src: "https://secure.archives.cafe/anny/45498742685.mp4#t=3836",
       poster: "https://secure.archives.cafe/anny/45498742685.jpg",
-      id: "vod_2"
+      id: "vod_6"
    },
    {
       name: "House Flipper",
       src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
       poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_3"
+      id: "vod_7"
    },
 ]
