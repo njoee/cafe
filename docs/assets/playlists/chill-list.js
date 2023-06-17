8e47a812ed11befc46817845d98cb4f1f4983580
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-17 10:24:20 -0400

    Update chill-list.js
    added stanley games, remove watched, update timestamps

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,50 +1,38 @@
 let allVideos = [
    {
-      name: "Megalovania 4h Ver.",
-      src: "https://secure.archives.cafe/anny/39345137339.mp4#t=6395",
-      poster: "https://secure.archives.cafe/anny/39345137339.jpg",
-      id: "vod_1"
-   },
-   {
-      name: "Undertale Gen Run 4",
-      src: "https://secure.archives.cafe/anny/39355600347.mp4#t=2333",
-      poster: "https://secure.archives.cafe/anny/39355600347.jpg",
-      id: "vod_2"
-   },
-   {
-      name: "Deltarune Part 1",
-      src: "https://secure.archives.cafe/anny/1226716557.mp4",
-      poster: "https://secure.archives.cafe/anny/1226716557.jpg",
-      id: "vod_3"
-   },
-   {
-      name: "Deltarune Part 2",
-      src: "https://secure.archives.cafe/anny/39429041207.mp4#t=4444",
-      poster: "https://secure.archives.cafe/anny/39429041207.jpg",
-      id: "vod_4"
-   },
-   {
       name: "Deltarune Part 3",
-      src: "https://secure.archives.cafe/anny/39431588679.mp4#t=4020",
+      src: "https://secure.archives.cafe/anny/39431588679.mp4#t=4990",
       poster: "https://secure.archives.cafe/anny/39431588679.jpg",
-      id: "vod_5"
+      id: "vod_1"
    },
    {
       name: "Deltarune Part 4",
       src: "https://secure.archives.cafe/anny/39438352343.mp4#t=2113",
       poster: "https://secure.archives.cafe/anny/39438352343.jpg",
-      id: "vod_6"
+      id: "vod_2"
+   },
+   {
+      name: "The Stanley Parable",
+      src: "https://secure.archives.cafe/anny/43526898317.mp4#t=1645",
+      poster: "https://secure.archives.cafe/anny/43526898317.jpg",
+      id: "vod_3"
+   },
+   {
+      name: "The Stanley Parable: Ultra Deluxe",
+      src: "https://secure.archives.cafe/anny/39290504039.mp4#t=2095",
+      poster: "https://secure.archives.cafe/anny/39290504039.jpg",
+      id: "vod_4"
    },
    {
       name: "Powerwash Simulator",
       src: "https://secure.archives.cafe/anny/45498742685.mp4#t=3836",
       poster: "https://secure.archives.cafe/anny/45498742685.jpg",
-      id: "vod_7"
+      id: "vod_5"
    },
    {
       name: "House Flipper",
       src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
       poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_8"
+      id: "vod_6"
    }
 ]
