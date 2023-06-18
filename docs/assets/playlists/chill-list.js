d6b747dbe51297365e8cd286af4e5144db1c74e1
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-18 10:06:04 -0400

    Update chill-list.js
    removed watched

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,38 +1,20 @@
 let allVideos = [
    {
-      name: "Deltarune Part 3",
-      src: "https://secure.archives.cafe/anny/39431588679.mp4#t=4990",
-      poster: "https://secure.archives.cafe/anny/39431588679.jpg",
-      id: "vod_1"
-   },
-   {
-      name: "Deltarune Part 4",
-      src: "https://secure.archives.cafe/anny/39438352343.mp4#t=2113",
-      poster: "https://secure.archives.cafe/anny/39438352343.jpg",
-      id: "vod_2"
-   },
-   {
-      name: "The Stanley Parable",
-      src: "https://secure.archives.cafe/anny/43526898317.mp4#t=1645",
-      poster: "https://secure.archives.cafe/anny/43526898317.jpg",
-      id: "vod_3"
-   },
-   {
       name: "The Stanley Parable: Ultra Deluxe",
-      src: "https://secure.archives.cafe/anny/39290504039.mp4#t=2095",
+      src: "https://secure.archives.cafe/anny/39290504039.mp4#t=7342",
       poster: "https://secure.archives.cafe/anny/39290504039.jpg",
-      id: "vod_4"
+      id: "vod_1"
    },
    {
       name: "Powerwash Simulator",
       src: "https://secure.archives.cafe/anny/45498742685.mp4#t=3836",
       poster: "https://secure.archives.cafe/anny/45498742685.jpg",
-      id: "vod_5"
+      id: "vod_2"
    },
    {
       name: "House Flipper",
       src: "https://secure.archives.cafe/anny/45513028141.mp4#t=12325",
       poster: "https://secure.archives.cafe/anny/45513028141.jpg",
-      id: "vod_6"
+      id: "vod_3"
    }
 ]
