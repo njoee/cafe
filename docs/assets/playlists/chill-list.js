719ae5cb82146cb711cec434a05738330178f60c
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-19 15:31:15 -0400

    Update chill-list.js
    added SOMA

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,28 +1,32 @@
 let allVideos = [
    {
-      name: "The Stanley Parable: Ultra Deluxe",
-      src: "https://secure.archives.cafe/anny/39290504039.mp4#t=7342",
-      poster: "https://secure.archives.cafe/anny/39290504039.jpg",
-      id: "vod_1"
-   },
-   {
-        name: "Portal (Part 1)",
-        src: "https://secure.archives.cafe/anny/41622345597-hl.mp4",
-        id: "vod_2"   
-   },
-   {
-        name: "Portal (Part 2)",
-        src: "https://secure.archives.cafe/anny/41635418893-hl.mp4",
-        id: "vod_3"   
-   },
-   {
         name: "Portal 2 (Part 1)",
-        src: "https://secure.archives.cafe/anny/41679706157.mp4#t=1620",
-        id: "vod_4"   
+        src: "https://secure.archives.cafe/anny/41679706157.mp4#t=12895",
+        id: "vod_1"   
    },
    {
         name: "Portal 2 (Part 2)",
         src: "https://secure.archives.cafe/anny/39310982331.mp4#t=2235",
-        id: "vod_5"   
+        id: "vod_2"   
+   },
+   {
+      name: "SOMA (Part 1)",
+      src: "https://secure.archives.cafe/anny/39310293287.mp4#t=1142",
+      id: "vod_3"
+   },
+   {
+      name: "SOMA (Part 2)",
+      src: "https://secure.archives.cafe/anny/39317699831.mp4#t=3668",
+      id: "vod_4"
+   },
+   {
+      name: "SOMA (Part 3)",
+      src: "https://secure.archives.cafe/anny/39801264263.mp4#t=3719",
+      id: "vod_5"
+   },
+   {
+      name: "SOMA (Part 4)",
+      src: "https://secure.archives.cafe/anny/39809289751.mp4#t=3210",
+      id: "vod_6"
    }
 ]
