3489172457176f1c9a26a69d55474fcdc219e1ff
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-20 17:26:29 -0400

    Update collab-list.js
    changed order and skipped chatting

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,19 +1,19 @@
 let allVideos = [
    {
-      name: "BIG BRAIN TIME WITH HAWOOKA (Escape Sim, Superliminal, Minecraft)",
-      src: "https://secure.archives.cafe/anny/44677238525.mp4#t=194",
-      poster: "https://secure.archives.cafe/anny/44677238525.jpg",
+      name: "look what Tob taught me peepoFinger (We Were Here)",
+      src: "https://secure.archives.cafe/anny/39935145031.mp4#t=2325",
+      poster: "https://secure.archives.cafe/anny/39935145031.jpg",
       id: "vod_1"
    },
    {
-      name: "look what Tob taught me peepoFinger (We Were Here)",
-      src: "https://secure.archives.cafe/anny/39935145031.mp4",
-      poster: "https://secure.archives.cafe/anny/39935145031.jpg",
+      name: "BIG BRAIN TIME WITH HAWOOKA (Escape Sim, Superliminal, Minecraft)",
+      src: "https://secure.archives.cafe/anny/44677238525.mp4#t=194",
+      poster: "https://secure.archives.cafe/anny/44677238525.jpg",
       id: "vod_2"
    },
    {
       name: "Mario Party Collab w/Arex, Bella, and Bobber",
-      src: "https://secure.archives.cafe/anny/44299085085.mp4",
+      src: "https://secure.archives.cafe/anny/44299085085.mp4#t=1668",
       poster: "https://secure.archives.cafe/anny/44299085085.jpg",
       id: "vod_3"
    },
