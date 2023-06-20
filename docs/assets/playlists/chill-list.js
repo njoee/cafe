5c65be3bf4ff64b9be65f758ae72fd3310df7b29
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-20 17:21:01 -0400

    Update chill-list.js
    add inside and limbo

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,32 +1,27 @@
 let allVideos = [
    {
-        name: "Portal 2 (Part 1)",
-        src: "https://secure.archives.cafe/anny/41679706157.mp4#t=12895",
-        id: "vod_1"   
-   },
-   {
-        name: "Portal 2 (Part 2)",
-        src: "https://secure.archives.cafe/anny/39310982331.mp4#t=2235",
-        id: "vod_2"   
-   },
-   {
-      name: "SOMA (Part 1)",
-      src: "https://secure.archives.cafe/anny/39310293287.mp4#t=1142",
-      id: "vod_3"
-   },
-   {
-      name: "SOMA (Part 2)",
-      src: "https://secure.archives.cafe/anny/39317699831.mp4#t=3668",
-      id: "vod_4"
-   },
-   {
       name: "SOMA (Part 3)",
-      src: "https://secure.archives.cafe/anny/39801264263.mp4#t=3719",
-      id: "vod_5"
+      src: "https://secure.archives.cafe/anny/39801264263.mp4#t=9020",
+      id: "vod_1"
    },
    {
       name: "SOMA (Part 4)",
       src: "https://secure.archives.cafe/anny/39809289751.mp4#t=3210",
-      id: "vod_6"
+      id: "vod_2"
+   },
+   {
+      name: "INSIDE (Part 1)",
+      src: "https://secure.archives.cafe/anny/43295077741.mp4#t=7177",
+      id: "vod_3"
+   },
+   {
+      name: "INSIDE (Part 2)",
+      src: "https://secure.archives.cafe/anny/43389693613.mp4#t=8501",
+      id: "vod_4"
+   },
+   {
+      name: "LIMBO (Part 1)",
+      src: "https://secure.archives.cafe/anny/43403500749.mp4#t=10965",
+      id: "vod_5"
    }
 ]
