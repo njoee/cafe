5abcc799f57eb65ccf08ea0fed0b670be7d383a3
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-22 19:59:48 -0400

    Update collab-list.js
    more minecraft

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,25 +1,27 @@
 let allVideos = [
    {
-      name: "look what Tob taught me peepoFinger (We Were Here)",
-      src: "https://secure.archives.cafe/anny/39935145031.mp4#t=6020",
-      poster: "https://secure.archives.cafe/anny/39935145031.jpg",
+      name: "Minecraft Vshojo Server (Part 7)",
+      src: "https://secure.archives.cafe/anny/44209167917.mp4#t=5256",
       id: "vod_1"
    },
    {
-      name: "BIG BRAIN TIME WITH HAWOOKA (Escape Sim, Superliminal, Minecraft)",
-      src: "https://secure.archives.cafe/anny/44677238525.mp4#t=194",
-      poster: "https://secure.archives.cafe/anny/44677238525.jpg",
+      name: "Fall Guys with frens",
+      src: "https://secure.archives.cafe/anny/39446240775.mp4#t=527",
       id: "vod_2"
    },
    {
-      name: "Mario Party Collab w/Arex, Bella, and Bobber",
-      src: "https://secure.archives.cafe/anny/44299085085.mp4#t=1668",
-      poster: "https://secure.archives.cafe/anny/44299085085.jpg",
+      name: "Minecraft Vshojo Server (Part 8)",
+      src: "https://secure.archives.cafe/anny/44380081357.mp4#t=2931",
       id: "vod_3"
    },
    {
-      name: "Minecraft Vshojo Server (Part 7)",
-      src: "https://secure.archives.cafe/anny/44209167917.mp4",
+      name: "Minecraft Vshojo Server (Part 9)",
+      src: "https://secure.archives.cafe/anny/44390899773.mp4#t=1184",
       id: "vod_4"
+   },
+   {
+      name: "Minecraft Vshojo Server (Part 9)",
+      src: "https://secure.archives.cafe/anny/44452533773.mp4#t=2750",
+      id: "vod_5"
    }
 ]
