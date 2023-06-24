b791c45b05e3bacd2468b1c494c8ba566e8a610f
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-24 17:00:13 -0400

    Update collab-list.js
    update timing, add vei, add multi collab

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,27 +1,17 @@
 let allVideos = [
    {
-      name: "Minecraft Vshojo Server (Part 7)",
-      src: "https://secure.archives.cafe/anny/44209167917.mp4#t=5256",
+      name: "Minecraft Vshojo Server (Part 10)",
+      src: "https://secure.archives.cafe/anny/44452533773.mp4#t=17878",
       id: "vod_1"
    },
    {
-      name: "Fall Guys with frens",
+      name: "Vei Collab",
       src: "https://secure.archives.cafe/anny/39446240775.mp4#t=527",
       id: "vod_2"
    },
    {
-      name: "Minecraft Vshojo Server (Part 8)",
-      src: "https://secure.archives.cafe/anny/44380081357.mp4#t=2931",
+      name: "Mods/Bella Collabs",
+      src: "https://secure.archives.cafe/anny/39446240775.mp4#t=527",
       id: "vod_3"
-   },
-   {
-      name: "Minecraft Vshojo Server (Part 9)",
-      src: "https://secure.archives.cafe/anny/44390899773.mp4#t=1184",
-      id: "vod_4"
-   },
-   {
-      name: "Minecraft Vshojo Server (Part 9)",
-      src: "https://secure.archives.cafe/anny/44452533773.mp4#t=2750",
-      id: "vod_5"
    }
 ]
