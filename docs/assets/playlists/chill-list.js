20315fa8b35b457a93199a7b01c68f751f9a0acf
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-25 15:07:56 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,12 +1,27 @@
 let allVideos = [
    {
-      name: "Mods/Bella Collabs",
-      src: "https://secure.archives.cafe/anny/39306891179.mp4#t=1230",
-      id: "vod_1"
+      name: "Predebut Bella Phasmo (1)",
+      src: "https://secure.archives.cafe/anny/40813094637.mp4?824780683053367306#t=7199",
+      id: "vod_2"
    },
    {
-      name: "Haruka Super Collab 2",
-      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=5496",
+      name: "Predebut Bella Phasmo (2)",
+      src: "https://secure.archives.cafe/anny/40832547101.mp4?824780683053367306#t=8914",
       id: "vod_2"
+   },
+   {
+      name: "Mods/Bella Apex/Phasmo for real actually",
+      src: "https://secure.archives.cafe/anny/42369044782.mp4#t=6460",
+      id: "vod_3"
+   },
+   {
+      name: "There is No Game (1)",
+      src: "https://secure.archives.cafe/anny/44540038397.mp4#t=5496",
+      id: "vod_4"
+   },
+   {
+      name: "There is No Game (2)",
+      src: "https://secure.archives.cafe/anny/44551900573.mp4#t=5496",
+      id: "vod_5"
    }
 ]
