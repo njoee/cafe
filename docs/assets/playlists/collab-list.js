ba319a31953813053db7fcd9e107bb03ba348cb8
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-25 05:33:25 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,17 +1,7 @@
 let allVideos = [
    {
-      name: "Minecraft Vshojo Server (Part 10)",
-      src: "https://secure.archives.cafe/anny/44452533773.mp4#t=17878",
+      name: "Mods/Bella for real actually",
+      src: "https://secure.archives.cafe/anny/42369044782.mp4#t=6460",
       id: "vod_1"
-   },
-   {
-      name: "Mods/Bella Collabs",
-      src: "https://secure.archives.cafe/anny/39306891179.mp4#t=1230",
-      id: "vod_2"
-   },
-   {
-      name: "Haruka Super Collab 2",
-      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=5496",
-      id: "vod_3"
    }
 ]
