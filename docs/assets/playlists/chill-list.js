4d071117035b9e91878a1fc5c7607fbb8e15f1ef
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-24 22:45:18 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,27 +1,12 @@
 let allVideos = [
    {
-      name: "SOMA (Part 3)",
-      src: "https://secure.archives.cafe/anny/39801264263.mp4#t=9020",
+      name: "Mods/Bella Collabs",
+      src: "https://secure.archives.cafe/anny/39306891179.mp4#t=1230",
       id: "vod_1"
    },
    {
-      name: "SOMA (Part 4)",
-      src: "https://secure.archives.cafe/anny/39809289751.mp4#t=3210",
+      name: "Haruka Super Collab 2",
+      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=5496",
       id: "vod_2"
-   },
-   {
-      name: "INSIDE (Part 1)",
-      src: "https://secure.archives.cafe/anny/43295077741.mp4#t=7177",
-      id: "vod_3"
-   },
-   {
-      name: "INSIDE (Part 2)",
-      src: "https://secure.archives.cafe/anny/43389693613.mp4#t=8501",
-      id: "vod_4"
-   },
-   {
-      name: "LIMBO (Part 1)",
-      src: "https://secure.archives.cafe/anny/43403500749.mp4#t=10965",
-      id: "vod_5"
    }
 ]
