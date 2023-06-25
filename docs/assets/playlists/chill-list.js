8d78f23bb543290377df3b8804285793121c2e83
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-25 15:19:49 -0400

    Update chill-list.js
    fixed IDs

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -2,7 +2,7 @@ let allVideos = [
    {
       name: "Predebut Bella Phasmo (1)",
       src: "https://secure.archives.cafe/anny/40813094637.mp4?824780683053367306#t=7199",
-      id: "vod_2"
+      id: "vod_1"
    },
    {
       name: "Predebut Bella Phasmo (2)",
