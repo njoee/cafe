5c2570966cbf3d77454829af8f36d010ccb7c447
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-24 21:02:04 -0400

    Update collab-list.js
    changed out vei collab

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -5,13 +5,13 @@ let allVideos = [
       id: "vod_1"
    },
    {
-      name: "Vei Collab",
-      src: "https://secure.archives.cafe/anny/42369044782.mp4#t=6460",
+      name: "Mods/Bella Collabs",
+      src: "https://secure.archives.cafe/anny/39306891179.mp4#t=1230",
       id: "vod_2"
    },
    {
-      name: "Mods/Bella Collabs",
-      src: "https://secure.archives.cafe/anny/39306891179.mp4#t=1230",
+      name: "Haruka Super Collab 2",
+      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=5496",
       id: "vod_3"
    }
 ]
