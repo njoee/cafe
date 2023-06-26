7d85e98ada7dd3495df6ce006c70919fc67133d5
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-26 16:45:43 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,32 +1,22 @@
 let allVideos = [
    {
-      name: "Predebut Bella Phasmo (1)",
-      src: "https://secure.archives.cafe/anny/40813094637.mp4?824780683053367306#t=7199",
-      id: "vod_1"
+      name: "Little Nightmares (Part 1)",
+      src: "https://secure.archives.cafe/anny/42143569438.mp4#t=733",
+      id: "vod_1"   
    },
    {
-      name: "Predebut Bella Phasmo (2)",
-      src: "https://secure.archives.cafe/anny/40832547101.mp4?824780683053367306#t=8913",
-      id: "vod_2"
+      name: "Little Nightmares (Part 2) + Little Nightmares II (Part 1)",
+      src: "https://secure.archives.cafe/anny/41237468157.mp4#t=2090",
+      id: "vod_2"   
+   },
+   {
+      name: "Little Nightmares II (Part 2)",
+      src: "https://secure.archives.cafe/anny/42220771934#t=2320.mp4",
+      id: "vod_3"   
    },
    {
       name: "Mods/Bella Apex/Phasmo for real actually",
       src: "https://secure.archives.cafe/anny/42369044782.mp4#t=6460",
-      id: "vod_3"
-   },
-   {
-      name: "Keep Talking and Nobody Explodes (w/Haruka)",
-      src: "https://secure.archives.cafe/anny/39348756391.mp4#t=2611",
       id: "vod_4"
-   },
-   {
-      name: "There is No Game (1)",
-      src: "https://secure.archives.cafe/anny/44540038397.mp4#t=5496",
-      id: "vod_5"
-   },
-   {
-      name: "There is No Game (2)",
-      src: "https://secure.archives.cafe/anny/44551900573.mp4#t=5496",
-      id: "vod_6"
    }
 ]
