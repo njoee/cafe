c2f25f6ce2a5ad6e062eed7fbca80466ee8de8e7
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-26 21:15:20 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -8,10 +8,5 @@ let allVideos = [
       name: "Predebut Bella Phasmo (2)",
       src: "https://secure.archives.cafe/anny/40832547101.mp4?824780683053367306#t=8914",
       id: "vod_2"
-   },
-   {
-      name: "Mods/Bella for real actually",
-      src: "https://secure.archives.cafe/anny/42369044782.mp4#t=6460",
-      id: "vod_3"
    }
 ]
