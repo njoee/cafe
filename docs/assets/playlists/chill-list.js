b0236b0c2657531024524e3cfc7d87a437ea1e0c
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-28 16:54:05 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,27 +1,22 @@
 let allVideos = [
    {
-      name: "Poppy Playtime & youtube react after",
-      src: "https://secure.archives.cafe/anny/44984096669.mp4#t=4750",
-      id: "vod_1"   
+      name: "Hollow Knight Part 1",
+      src: "https://secure.archives.cafe/anny/43921044045.mp4#t=1018",
+      id: "vod_1"
    },
    {
-      name: "Internet Cafe Simulator",
-      src: "https://secure.archives.cafe/anny/45364046013.mp4#t=9457",
-      id: "vod_2"   
+      name: "React & Solo Terraria (@3h9m) June 2021",
+      src: "https://secure.archives.cafe/anny/39572562363.mp4#t=4134",
+      id: "vod_2"
    },
    {
-      name: "The Last Campfire",
-      src: "https://secure.archives.cafe/anny/45557667501.mp4#t=1892",
-      id: "vod_3"   
+      name: "osu! and Solo Terraria July 2022",
+      src: "https://secure.archives.cafe/anny/39486783623.mp4#t=739",
+      id: "vod_3"
    },
    {
-      name: "Overwatch 2 w/Bella & youtube react after",
-      src: "https://secure.archives.cafe/anny/39954890791.mp4#t=17343",
-      id: "vod_4"
-   },
-   {
-      name: "Crab Game, PUBG, Honkai Star Rail",
+      name: "Crab Game, PUBG, Honkai Star Rail Feb 2023",
       src: "https://secure.archives.cafe/anny/40261277367.mp4#t=4884",
-      id: "vod_5"
+      id: "vod_4"
    }
 ]
