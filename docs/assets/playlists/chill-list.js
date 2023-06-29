d2e440604721c9dcc4aefc5439c7c88bc10b11ed
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-29 17:51:58 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,21 +1,21 @@
 let allVideos = [
    {
-      name: "Hollow Knight Part 1",
-      src: "https://secure.archives.cafe/anny/43921044045.mp4#t=1018",
+      name: "osu! and Solo Terraria July 2022 (1)",
+      src: "https://secure.archives.cafe/anny/39486783623.mp4#t=8917",
       id: "vod_1"
    },
    {
-      name: "React & Solo Terraria (@3h9m) June 2021",
-      src: "https://secure.archives.cafe/anny/39572562363.mp4#t=4134",
+      name: "Solo Terraria July 2022 (2)",
+      src: "https://secure.archives.cafe/anny/39495471943.mp4#t=3654",
       id: "vod_2"
    },
    {
-      name: "osu! and Solo Terraria July 2022",
-      src: "https://secure.archives.cafe/anny/39486783623.mp4#t=739",
+      name: "Solo Terraria July 2022 (3)",
+      src: "https://secure.archives.cafe/anny/39506160375.mp4#t=2533",
       id: "vod_3"
    },
    {
-      name: "Crab Game, PUBG, Honkai Star Rail Feb 2023",
+      name: "Crab Game, PUBG, stops before Star Rail Feb 2023",
       src: "https://secure.archives.cafe/anny/40261277367.mp4#t=4884,13876",
       id: "vod_4"
    }
