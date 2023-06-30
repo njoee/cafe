db3d50f84c749a3b354b6a23c1ac1e9164541a5c
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-30 10:56:28 -0400

    Update chill-list.js
    typo fix

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,4 +1,5 @@
-let allVideos = [   {
+let allVideos = [
+   {
       name: "Solo Terraria July 2022 (4)",
       src: "https://secure.archives.cafe/anny/39509061991.mp4#t=2400",
       id: "vod_1"
@@ -17,11 +18,10 @@ let allVideos = [   {
       name: "Terraria August 2022 (Last Part of July Series?)",
       src: "https://secure.archives.cafe/anny/39538620823.mp4#t=1800",
       id: "vod_4"
-   }
+   },
    {
       name: "Crab Game, PUBG, stops before Star Rail Feb 2023",
       src: "https://secure.archives.cafe/anny/40261277367.mp4#t=5084,13876",
       id: "vod_5"
-   },
-
+   }
 ]
