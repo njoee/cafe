b681d4fa0c455ee7e80eb353b8d154da414df616
joey☕ <79419005+njoee@users.noreply.github.com>
2023-06-30 10:51:54 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,22 +1,27 @@
-let allVideos = [
-   {
-      name: "osu! and Solo Terraria July 2022 (1)",
-      src: "https://secure.archives.cafe/anny/39486783623.mp4#t=8917",
+let allVideos = [   {
+      name: "Solo Terraria July 2022 (4)",
+      src: "https://secure.archives.cafe/anny/39509061991.mp4#t=2400",
       id: "vod_1"
    },
    {
-      name: "Solo Terraria July 2022 (2)",
-      src: "https://secure.archives.cafe/anny/39495471943.mp4#t=3654",
+      name: "Solo Terraria July 2022 (5)",
+      src: "https://secure.archives.cafe/anny/39511172887.mp4#t=2700",
       id: "vod_2"
    },
    {
-      name: "Solo Terraria July 2022 (3)",
-      src: "https://secure.archives.cafe/anny/39506160375.mp4#t=2533",
+      name: "Solo Terraria July 2022 (6)",
+      src: "https://secure.archives.cafe/anny/39521982247.mp4#t=4200",
       id: "vod_3"
    },
    {
-      name: "Crab Game, PUBG, stops before Star Rail Feb 2023",
-      src: "https://secure.archives.cafe/anny/40261277367.mp4#t=4884,13876",
+      name: "Terraria August 2022 (Last Part of July Series?)",
+      src: "https://secure.archives.cafe/anny/39538620823.mp4#t=1800",
       id: "vod_4"
    }
+   {
+      name: "Crab Game, PUBG, stops before Star Rail Feb 2023",
+      src: "https://secure.archives.cafe/anny/40261277367.mp4#t=5084,13876",
+      id: "vod_5"
+   },
+
 ]
