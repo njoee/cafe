477a4fdbcf41a9c87c3a68dbb366889033ff03ff
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-01 20:51:35 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -1,32 +1,22 @@
 let allVideos = [
    {
-      name: "Solo Terraria July 2022 (4)",
-      src: "https://secure.archives.cafe/anny/39509061991.mp4#t=3918",
+      name: "Hollow Knight (3)",
+      src: "https://secure.archives.cafe/anny/44045697229.mp4#t=4458",
       id: "vod_1"
    },
    {
-      name: "Solo Terraria July 2022 (5)",
-      src: "https://secure.archives.cafe/anny/39511172887.mp4#t=2602",
+      name: "Hollow Knight (final)",
+      src: "https://secure.archives.cafe/anny/44057016685.mp4#t=6722",
       id: "vod_2"
    },
    {
-      name: "Solo Terraria July 2022 (6)",
-      src: "https://secure.archives.cafe/anny/39521982247.mp4#t=4204",
+      name: "getfixedboi Terraria Oct 2022 (1)",
+      src: "https://secure.archives.cafe/anny/39760345287.mp4#t=1833",
       id: "vod_3"
    },
    {
-      name: "Terraria August 2022 (Last Part of July Series)",
-      src: "https://secure.archives.cafe/anny/39538754375.mp4#t=15080",
-      id: "vod_4"
-   },
-   {
-      name: "Modded Terraria(?) Oct 2022 (1)",
-      src: "https://secure.archives.cafe/anny/39760345287.mp4#t=1833",
-      id: "vod_5"
-   },
-   {
-      name: "Modded Terraria(?) Oct 2022 (2)",
+      name: "getfixedboi Terraria Oct 2022 (2)",
       src: "https://secure.archives.cafe/anny/39765276519.mp4#t=2077",
-      id: "vod_6"
+      id: "vod_2"
    }
 ]
