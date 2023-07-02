2dab935207aa0aaf58fc3b181fd4f6408469e32d
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-02 18:07:48 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,22 +1,27 @@
 let allVideos = [
    {
-      name: "getfixedboi Terraria Oct 2022 (1)",
-      src: "https://secure.archives.cafe/anny/39760345287.mp4#t=1833",
+      name: "Hollow Knight (final)",
+      src: "https://secure.archives.cafe/anny/44057016685.mp4#t=9781",
       id: "vod_1"
    },
    {
-      name: "getfixedboi Terraria Oct 2022 (2)",
-      src: "https://secure.archives.cafe/anny/39765276519.mp4#t=2077",
+      name: "Sonic Frontiers",
+      src: "https://secure.archives.cafe/anny/39836038327.mp4#t=2904",
       id: "vod_2"
    },
    {
-      name: "Hollow Knight (3)",
-      src: "https://secure.archives.cafe/anny/44045697229.mp4#t=4458",
+      name: "Sonic Colors/Mario World",
+      src: "https://secure.archives.cafe/anny/43621911981.mp4#t=1666",
       id: "vod_3"
    },
    {
-      name: "Hollow Knight (final)",
-      src: "https://secure.archives.cafe/anny/44057016685.mp4#t=6722",
+      name: "Haruka Gartic+Tetris Collab",
+      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=5652",
       id: "vod_4"
+   },
+   {
+      name: "Tob VR Collab",
+      src: "https://secure.archives.cafe/anny/39478824455.mp4#t=661",
+      id: "vod_5"
    }
 ]
