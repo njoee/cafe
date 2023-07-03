9c6f0f3242acca3cfffdab87798c346b42f888d3
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-03 10:07:00 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,27 +1,12 @@
 let allVideos = [
    {
-      name: "Hollow Knight (final)",
-      src: "https://secure.archives.cafe/anny/44057016685.mp4#t=9781",
-      id: "vod_1"
-   },
-   {
-      name: "Sonic Frontiers",
-      src: "https://secure.archives.cafe/anny/39836038327.mp4#t=2904",
-      id: "vod_2"
-   },
-   {
-      name: "Sonic Colors/Mario World",
-      src: "https://secure.archives.cafe/anny/43621911981.mp4#t=1666",
-      id: "vod_3"
-   },
-   {
       name: "Haruka Gartic+Tetris Collab",
-      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=5652",
-      id: "vod_4"
+      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=10965",
+      id: "vod_1"
    },
    {
       name: "Tob VR Collab",
       src: "https://secure.archives.cafe/anny/39478824455.mp4#t=661",
-      id: "vod_5"
+      id: "vod_2"
    }
 ]
