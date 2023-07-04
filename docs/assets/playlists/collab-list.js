1763f1701035670766f5b34bb7ace50fd69c55ff
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-04 19:30:06 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,22 +1,22 @@
 let allVideos = [
    {
-      name: "Haruka Gartic+Tetris Collab (Nov 2022)",
-      src: "https://secure.archives.cafe/anny/39826221447.mp4#t=11240",
-      id: "vod_1"
-   },
-   {
-      name: "Tob VR Collab (July 2022)",
-      src: "https://secure.archives.cafe/anny/39478824455.mp4#t=661",
-      id: "vod_2"
-   },
-   {
       name: "Minecraft VSJ Server Oct 2021 (1)",
-      src: "https://secure.archives.cafe/anny/44128238717.mp4#t=2105",
-      id: "vod_3"
+      src: "https://secure.archives.cafe/anny/44128238717.mp4#t=13560",
+      id: "vod_1"
    },
    {
       name: "Minecraft VSJ Server Oct 2021 (2)",
       src: "https://secure.archives.cafe/anny/44139693309.mp4#t=398",
+      id: "vod_2"
+   },
+   {
+      name: "Minecraft VSJ Server Oct 2021 (3)",
+      src: "https://secure.archives.cafe/anny/44150764717.mp4#t=582",
+      id: "vod_3"
+   },
+   {
+      name: "Minecraft VSJ Server Oct 2021 (4)",
+      src: "https://secure.archives.cafe/anny/44162078157.mp4#t=378",
       id: "vod_4"
    }
 ]
