d6a361b64c79f99b850ff1a7b720822b1760c6ec
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-05 21:21:02 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,22 +1,17 @@
 let allVideos = [
    {
-      name: "Minecraft VSJ Server Oct 2021 (1)",
-      src: "https://secure.archives.cafe/anny/44128238717.mp4#t=13560",
+      name: "Minecraft VSJ Server Oct 2021 (4)",
+      src: "https://secure.archives.cafe/anny/44162078157.mp4#t=3126",
       id: "vod_1"
    },
    {
-      name: "Minecraft VSJ Server Oct 2021 (2)",
-      src: "https://secure.archives.cafe/anny/44139693309.mp4#t=398",
+      name: "Minecraft VSJ Server Oct 2021 (5)",
+      src: "https://secure.archives.cafe/anny/44174758413.mp4#t=968",
       id: "vod_2"
    },
    {
-      name: "Minecraft VSJ Server Oct 2021 (3)",
-      src: "https://secure.archives.cafe/anny/44150764717.mp4#t=582",
+      name: "Minecraft VSJ Server Oct 2021 (6)",
+      src: "https://secure.archives.cafe/anny/44198241165.mp4#t=516",
       id: "vod_3"
-   },
-   {
-      name: "Minecraft VSJ Server Oct 2021 (4)",
-      src: "https://secure.archives.cafe/anny/44162078157.mp4#t=378",
-      id: "vod_4"
    }
 ]
