7d3e3e67cc830873c0cd396b3a3a3188934d2d0d
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-09 19:57:21 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,22 +1,17 @@
 let allVideos = [
    {
-      name: "First Time Ring Fit + Rabi Ribi",
-      src: "https://secure.archives.cafe/anny/43213281101.mp4#t=663",
-      id: "vod_1"
+      name: "Minecraft VSJ Server Oct 2021 (9)",
+      src: "https://secure.archives.cafe/anny/44390899773.mp4#t=7630",
+      id: "vod_4"
    },
    {
-      name: "Untitled Goose Game",
-      src: "https://secure.archives.cafe/anny/44656209213.mp4#t=972",
+      name: "Minecraft VSJ Exploring with John (final)",
+      src: "https://secure.archives.cafe/anny/44452533773.mp4#t=2772",
       id: "vod_2"
    },
    {
-      name: "Minecraft VSJ Server Oct 2021 (8)",
-      src: "https://secure.archives.cafe/anny/44380081357.mp4#t=2894",
+      name: "Haruka Collab (Minecraft)",
+      src: "https://secure.archives.cafe/anny/44677238525.mp4#t=195",
       id: "vod_3"
-   },
-   {
-      name: "Minecraft VSJ Server Oct 2021 (9)",
-      src: "https://secure.archives.cafe/anny/44390899773.mp4#t=1180",
-      id: "vod_4"
    }
 ]
