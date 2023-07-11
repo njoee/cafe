6dbe4f8dc53e462e3515fe3746a3d47dd7fb07b8
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-10 21:12:14 -0400

    Update collab-list.js

--- docs/assets/playlists/collab-list.js
+++ docs/assets/playlists/collab-list.js
@@ -1,17 +1,17 @@
 let allVideos = [
    {
-      name: "Minecraft VSJ Server Oct 2021 (9)",
-      src: "https://secure.archives.cafe/anny/44390899773.mp4#t=7630",
-      id: "vod_4"
+      name: "Haruka Collab (Escape Sim, Superliminal Multi, Minecraft)",
+      src: "https://secure.archives.cafe/anny/44677238525.mp4#t=1890",
+      id: "vod_1"
    },
    {
-      name: "Minecraft VSJ Exploring with John (final)",
-      src: "https://secure.archives.cafe/anny/44452533773.mp4#t=2772",
+      name: "Superlinimal (Solo)",
+      src: "https://secure.archives.cafe/anny/44368611229.mp4#t=1559",
       id: "vod_2"
    },
    {
-      name: "Haruka Collab (Minecraft)",
-      src: "https://secure.archives.cafe/anny/44677238525.mp4#t=195",
+      name: "Stray",
+      src: "https://secure.archives.cafe/anny/42274262347-stray.mp4",
       id: "vod_3"
    }
 ]
