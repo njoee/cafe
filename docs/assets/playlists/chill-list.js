3dcfd98a653909971555d125d7d93e27606866fe
joey☕ <79419005+njoee@users.noreply.github.com>
2023-07-11 00:23:07 -0400

    Update chill-list.js

--- docs/assets/playlists/chill-list.js
+++ docs/assets/playlists/chill-list.js
@@ -2,21 +2,16 @@ let allVideos = [
    {
       name: "Superlinimal (Solo)",
       src: "https://secure.archives.cafe/anny/44368611229.mp4#t=1559",
-      id: "vod_2"
+      id: "44368611229"
    },
    {
       name: "Elden Ring 2h Mittens Video (uploading)",
       src: "https://secure.archives.cafe/anny/42274262347-stray.mp4",
-      id: "vod_3"
-   },
-   {
-      name: "Stray",
-      src: "https://secure.archives.cafe/anny/42274262347-stray.mp4",
-      id: "vod_3"
+      id: "42274262347"
    },
    {
       name: "Elden Ring P1",
-      src: "https://secure.archives.cafe/anny/ER1.mp4",
-      id: "vod_3"
-   },
+      src: "https://secure.archives.cafe/anny/42303897387-ER1.mp4",
+      id: "42303897387_1"
+   }
 ]
